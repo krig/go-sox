7eef02fb7a76d7cfeccebe13ab35c21b3c8ef071
Yannick Saive <ykse999@gmail.com>
2017-05-10 11:48:11 -0300

    fixed example4

--- examples/example4.go
+++ examples/example4.go
@@ -1,15 +1,18 @@
 // A simple example of using SoX libraries
 package main
 
-// Use this URL to import the go-sox library
-import "github.com/krig/go-sox"
-import "log"
-import "flag"
+import (
+	"flag"
+	"log"
+
+	// Use this URL to import the go-sox library
+	"github.com/krig/go-sox"
+)
 
 const (
 	//The (maximum) number of samples that we shall read/write at a time;
 	// chosen as a rough match to typical operating system I/O buffer size:
-	MAX_SAMPLES = 2048
+	MAX_READ_SAMPLES = 2048
 )
 
 func check(cond bool, test string) {
@@ -18,15 +21,13 @@ func check(cond bool, test string) {
 	}
 }
 
-// Concatenate audio files.  Note that the files must have the same number
-// of channels and the same sample rate.
-//
-// Usage: example4 input-1 input-2 [... input-n] output
+// Reads input file and displays a few seconds of wave-form, starting from
+// a given time through the audio.   E.g. example2 song2.au 30.75 1
 func main() {
 	flag.Parse()
 
 	// Need at least 2 input files + 1 output file.
-	if flag.NArg() < 2 + 1 {
+	if flag.NArg() < 2+1 {
 		log.Fatal("Usage: <input1> <input2> ... <output>")
 	}
 
@@ -36,19 +37,22 @@ func main() {
 	// Make sure to call Quit before terminating
 	defer sox.Quit()
 
-	var input *sox.Signal
-	var output *sox.Signal
+	var input *sox.Format
+	var output *sox.Format
+	var signal *sox.SignalInfo
 
 	// For each input file...
-	for i := 0; i < flag.NArg() - 1; i++ {
-		var samples [MAX_SAMPLES]Sample
+	for i := 0; i < flag.NArg()-1; i++ {
+		//initialize the Slice, such that when we pass it to C, it is an Array with sufficient Space
+		samples := make([]sox.Sample, MAX_READ_SAMPLES)
+
 		input = sox.OpenRead(flag.Arg(i))
 		if input == nil {
 			log.Fatal("OpenRead failed")
 		}
 		// If this is the first input file...
 		if i == 0 {
-			output = sox.OpenWrite(flag.Arg(flag.NArg() - 1),
+			output = sox.OpenWrite(flag.Arg(flag.NArg()-1),
 				input.Signal(), input.Encoding(),
 				nil)
 			if output == nil {
@@ -57,12 +61,17 @@ func main() {
 			defer output.Release()
 			signal = input.Signal()
 		} else {
+			// If this is not the first input file, the number of Channels
+			// and the Rate must match that of the first file (WHY?)
 			check(input.Signal().Channels() == signal.Channels(), "channels")
 			check(input.Signal().Rate() == signal.Rate(), "rate")
 		}
+
 		// Continue here!
-		for number_read := input.Read(samples); number_read != 0; number_read = input.Read(samples) {
-			check(output.Write(samples) == number_read, "write")
+		for number_read := input.Read(samples, MAX_READ_SAMPLES); //
+		number_read > 0;                                          //
+		number_read = input.Read(samples, MAX_READ_SAMPLES) {
+			check(output.Write(samples, uint(number_read)) == number_read, "write")
 		}
 		input.Release()
 	}
